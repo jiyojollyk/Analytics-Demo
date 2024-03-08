cdb4b5e47ade2b17680a6be0e1b977f7cd8a6552
jiyojollyk <jiyo.jolly@experionglobal.com>
2024-03-08 12:37:31 +0530

    change

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ const Home =()=>{
             <h2>Ecommerce Store</h2>
             <a href="https://youtu.be/nUQEo-HXZgc?si=G_tSDc21aoS4LrI6" target="_blank" className="yt-btn">Youtube</a>
             <div>
-                <h5>Subscribe to our newsletters</h5>
+                <h5>Subscribe to our newsletter</h5>
                 <form action="/thanks">
                     <input type="email" required />
                     <button>Submit</button>
